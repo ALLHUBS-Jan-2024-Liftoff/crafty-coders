91d13e348138e23b936e7184ed904a98c5569a71
ria <kristyneka@gmail.com>
2024-08-08 20:34:14 -0500

    searchbar

--- LaunchCash-Frontend/src/App.jsx
+++ LaunchCash-Frontend/src/App.jsx
@@ -4,21 +4,28 @@ import profile from "./pages/profile";
 import NavBar from './pages/NavBar'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { SearchBar } from "./components/SearchBar";
 function App() {
   return (
     <div className="App">
+        <div className="search-bar-container">
+        <SearchBar />
+        <div>SearchResults</div>
+    </div>
+
+
       <Router>
     <NavBar />
         <Routes>
           <Route path="/" element={<LoginRegister />} />
             <Route path="/profile" element={<profile />} />
-            <Route path="/transaction" component={Transaction} />
-            <Route path="/friends" component={Friends} />
-               <Route path="/profile" component={Profile} />
+{/*             <Route path="/transaction" component={Transaction} /> */}
+{/*             <Route path="/friends" component={Friends} /> */}
+{/*                <Route path="/profile" component={Profile} /> */}
         </Routes>
 
       </Router>
-      </div>
+ </div>
   )
 }
 
